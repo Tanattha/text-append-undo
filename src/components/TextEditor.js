680e0f5b516518ac6e63e677497b63e17bd20353
Tanattha <tanattha.thuenim@gmail.com>
2020-10-07 22:48:21 -0400

    first commit

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React from "react";
 
 class TextEditor extends React.Component {
+  state = {
+    input: "",
+    list: [],
+  };
+
+  handleonChange = (e) => {
+    e.preventDefault();
+    this.setState({ input: e.target.value });
+  };
+
+  handleonSubmit = (e) => {
+    e.preventDefault();
+    const newInput = this.state.input;
+    this.setState({ list: [...this.state.list, newInput], input: "" });
+  };
+
+  handleUndo = () => {
+    this.setState({
+      list: [...this.state.list].slice(0, -1),
+    });
+  };
 
   render() {
+    const { input, list } = this.state;
+
     return (
       <React.Fragment>
         <div className="controls">
-          <input className="word-input" type="text" data-testid="word-input" />
-          <button data-testid="append-button">Append</button>
-          <button data-testid="undo-button">Undo</button>
+          <input
+            value={input}
+            className="word-input"
+            type="text"
+            data-testid="word-input"
+            onChange={this.handleonChange}
+          />
+
+          <button
+            disabled={!input}
+            data-testid="append-button"
+            onClick={this.handleonSubmit}
+          >
+            Append
+          </button>
+          <button
+            data-testid="undo-button"
+            disabled={list.length === 0}
+            onClick={this.handleUndo}
+          >
+            Undo
+          </button>
         </div>
-        <div className="text-field" data-testid="text-field">REPLACE ME WITH CURRENT TEXT CONTENT</div>
+
+        <div className="text-field" data-testid="text-field">
+          {list.map((obj, index) => (
+            <li key={index}>{obj}&nbsp;</li>
+          ))}
+        </div>
       </React.Fragment>
     );
   }
